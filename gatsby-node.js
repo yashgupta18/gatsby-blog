76afcd44dcccfa1906242bcfb18d90c61c79e3f4
Yash Gupta <18guptayash@gmail.com>
2021-07-18 17:39:05 +0530

    Added styles, graphql query

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,8 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/node-apis/
- */
+const path=require('path')
+// For absolute imports
 
-// You can delete this file if you're not using it
+exports.onCreateWebpackConfig = ({ actions })=> {
+    actions.setWebpackConfig({
+        resolve:{modules:[path.resolve(__dirname,"src"), "node_modules"]},
+    })
+}
---
+++ src/components/Layout/index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { ThemeProvider } from 'styled-components'
+
+//styles
+import { GlobalStyles, lightTheme, darkTheme } from 'styles/GlobalStyles'
+//Hooks
+import {useMetaDataQuery}  from "hooks/useMetaDataQuery"
+
+const Layout = ({ children }) => {
+    const data=useMetaDataQuery()
+    console.log(data);
+    return (
+        <ThemeProvider theme={darkTheme}>
+            <GlobalStyles />
+            Layout Component
+            {children}
+        </ThemeProvider>
+    )
+}
+
+export default Layout
