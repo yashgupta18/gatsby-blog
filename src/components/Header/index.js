f46030801775641a755da93930deffddbd6e785c
Yash Gupta <18guptayash@gmail.com>
2021-07-25 13:46:42 +0530

    sidebar for mobile view added

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,7 +3,7 @@ import { Link } from 'gatsby'
 //Components
 import Menu from 'components/Menu'
 import Hamburger from 'components/Hamburger'
-
+import MobileMenu from 'components/MobileMenu'
 //styles
 import {Wrapper, Logo} from './Header.styles'
 //Hooks
@@ -14,6 +14,7 @@ const Header = ({siteTitle=``}) => {
     return (
 		<Wrapper>
 			<Hamburger menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+			<MobileMenu menuOpen={menuOpen} items={siteConfig.menu} />
 			<Menu items={siteConfig.menu} />
 			<Link to="/">
 				<Logo src={siteConfig.logo.publicURL} alt={siteTitle} />
---
+++ src/components/MobileMenu/index.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Link } from 'gatsby'
+//styles
+import {OverrideGlobalStyle, Wrapper,Nav} from './MobileMenu.styles'
+const MobileMenu = ({menuOpen, items}) => {
+    return (
+        <>
+            <OverrideGlobalStyle menuOpen={menuOpen} />
+			<Wrapper menuOpen={menuOpen}>
+				<Nav>
+					{items.map((item) => (
+						<li key={item.id}>
+							<Link to={item.link} activeClassName="active">{ item.name}</Link>
+						</li>
+					))}
+				</Nav>
+            </Wrapper>
+        </>
+    )
+}
+
+export default MobileMenu
